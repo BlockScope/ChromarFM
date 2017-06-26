a98d13e1a4e354d5116094e20ae0abf4e065051a
azardilis <azardilis@gmail.com>
2017-06-26 16:35:19 +0100

    new format for rules and style changes

--- app/Main.hs
+++ app/Main.hs
@@ -3,21 +3,18 @@
 
 module Main where
 
-import Graphics.Rendering.Chart.Easy hiding (at)
-import Graphics.Rendering.Chart.Backend.Diagrams(toFile)
 import GHC.Exts
 import Chromar
 import Env
 import Params
 import Photo
 
-
 tend = 700
 
-thrmFinal = at thr tend 
+thrmFinal = at thr tend
 
 isLeaf :: Agent -> Bool
-isLeaf (Leaf{}) = True
+isLeaf (Leaf {}) = True
 isLeaf _ = False
 
 pCron :: Fluent Double
@@ -27,10 +24,14 @@ pCron = when (thr <>*> (constant 465)) adPCron `orElse` juvPCron
     adPCron = constant 11.9
 
 lastTa :: Multiset Agent -> Double
-lastTa mix = head (sortWith Down [ta | (Leaf{i=i,m=_, a=_, ta=ta}, _) <- mix])
+lastTa mix = head (sortWith Down
+                   [ta | (Leaf{i=i,m=_, a=_, ta=ta}, _) <- mix])
 
-lastThr = Observable { name = "lastAppear",
-                       gen = lastTa }
+lastThr =
+    Observable
+    { name = "lastAppear"
+    , gen = lastTa
+    }
 
 rateApp lta pc thrt =
     if thrt - lta > pc
@@ -43,8 +44,11 @@ nLeaves mix =
         [ 1
         | (Leaf {}, _) <- mix ]
 
-nL = Observable { name ="nLeaves",
-                  gen = fromIntegral . nLeaves }
+nL =
+    Observable
+    { name = "nLeaves"
+    , gen = fromIntegral . nLeaves
+    }
 
 maxLeaf :: Multiset Agent -> Int
 maxLeaf mix = max ((i . head) $ sortLeaves) 2
@@ -52,15 +56,21 @@ maxLeaf mix = max ((i . head) $ sortLeaves) 2
     leaves = map fst $ select isLeaf mix
     sortLeaves = sortWith (Down . a) leaves
 
-maxL = Observable { name = "maxL",
-                    gen = fromIntegral . maxLeaf }
+maxL =
+    Observable
+    { name = "maxL"
+    , gen = fromIntegral . maxLeaf
+    }
 
 isRoot :: Agent -> Bool
-isRoot Root{} = True
+isRoot Root {} = True
 isRoot _ = False
 
-rootMass = Observable { name = "rootMass",
-                        gen = sumM m . select isRoot }
+rootMass =
+    Observable
+    { name = "rootMass"
+    , gen = sumM m . select isRoot
+    }
 
 getAngle :: Int -> Int -> Int -> Double
 getAngle i iMax nl
@@ -71,8 +81,8 @@ getAngle i iMax nl
 
 rosArea :: Multiset Agent -> Double
 rosArea mix
-  | nl <= 15 = sum lAreas
-  | otherwise = sum $ take 13 (sortWith Down lAreas )
+    | nl <= 15 = sum lAreas
+    | otherwise = sum $ take 13 (sortWith Down lAreas)
   where
     lAreas =
         [ a * ang
@@ -84,11 +94,17 @@ rosArea mix
     iMax = maxLeaf mix
     toRad d = d / 180 * pi
 
-rArea = Observable { name = "rosArea",
-                     gen = rosArea }
+rArea =
+    Observable
+    { name = "rosArea"
+    , gen = rosArea
+    }
 
-raArea = Observable { name = "trueArea",
-                      gen = sumM a . select isLeaf }
+raArea =
+    Observable
+    { name = "trueArea"
+    , gen = sumM a . select isLeaf
+    }
 
 dassim phR ra = phR * ra * gc
   where
@@ -120,10 +136,11 @@ rdem thrt = dem / maxDem
     tr = rootLc * thrmFinal
     a = 13.03
     b = 9.58
-    dem = ((thrt + 0.5) / tr)**(a-1) *
-          (1 - (thrt + 0.5) / tr)**(b-1)
-    maxPoint = tr/(1+(b-1)/(a-1))-0.5
-    maxDem = ((maxPoint+0.5)/tr)**(a-1)*(1-(maxPoint+0.5)/tr)**(b-1)
+    dem = ((thrt + 0.5) / tr) ** (a - 1) * (1 - (thrt + 0.5) / tr) ** (b - 1)
+    maxPoint = tr / (1 + (b - 1) / (a - 1)) - 0.5
+    maxDem =
+        ((maxPoint + 0.5) / tr) ** (a - 1) *
+        (1 - (maxPoint + 0.5) / tr) ** (b - 1)
 
 texp :: Int -> Double
 texp i
@@ -139,7 +156,7 @@ rm2c m = m * rootFactor
 rc2m c = c / rootFactor
 
 maint :: Double -> Double -> Int -> Obs -> Obs -> Double -> Double
-maint m a i iMax nl tempt = rlRes * leafArea * (1/24.0)
+maint m a i iMax nl tempt = rlRes * leafArea * (1 / 24.0)
   where
     p = 0.085
     p' = 0.016
@@ -147,7 +164,7 @@ maint m a i iMax nl tempt = rlRes * leafArea * (1/24.0)
     toRad d = d / 180 * pi
     ang = toRad $ getAngle i (floor iMax) (floor nl)
     leafArea = a * (cos ang)
-    rl20 = (p*c + p')*24
+    rl20 = (p * c + p') * 24
     rlRes = rl20 * exp ((actE * (tempt - 20)) / (293 * 8.314 * (tempt + 273)))
 
 -- growth in grams of mass for a leaf with:
@@ -197,50 +214,86 @@ $(return [])
 
 ----- rules -------
 
-growth = [rule| Leaf{i=i, m=m, a=a, ta=ta}, Cell{c=c, s=s'} -->
-                Leaf{m=m+(c2m $ g leafMass), a=max a ((sla' thr) * (m+g leafMass))},
-                Cell{c=c-1.24*(g leafMass), s=s'} @ldem i ta thr [c-1.24*(g leafMass) > 0.05*rArea] |]
+growth =
+    [rule|
+      Leaf{i=i, m=m, a=a, ta=ta}, Cell{c=c, s=s'} -->
+      Leaf{m=m+(c2m $ gr), a=max a a'}, Cell{c=c-grRes, s=s'}
+      @ld [c-grRes > cEqui]
+        where
+          gr = g leafMass,
+          a' = (sla' thr) * (m + gr),
+          ld = ldem i ta thr,
+          grRes = 1.24 * gr,
+          cEqui = 0.05 * rArea
+    |]
 
-assim = [rule| Cell{c=c, s=s'} -->
-               Cell{c=c + 0.875*dassim (phRate temp) rArea,
-                    s=s'+ 0.125*dassim (phRate temp) rArea}
-               @1.0 [day] |]
+assim =
+  [rule|
+    Cell{c=c, s=s'} -->
+    Cell{c=c + 0.875*da, s=s'+ 0.125*da}
+    @1.0 [day]
+      where
+        da = dassim (phRate temp) rArea
+  |]
 
-starchConv = [rule| Cell{c=c, s=s'} -->
-                    Cell{c=c+(s2c s'), s=s'-(s2c s')} @1.0 [not day] |]
+starchConv =
+  [rule|
+    Cell{c=c, s=s'} -->
+    Cell{c=c+(s2c s'), s=s'-(s2c s')} @1.0 [not day]
+  |]
 
 leafCr =
-    [rule| Cell{s=s'} --> Cell{s=s'},Leaf{i=(floor nL+1), ta=thr, m=cotArea/slaCot, a=cotArea}
-                                          @(rateApp lastThr pCron thr) |]
+    [rule|
+      Cell{s=s'} --> Cell{s=s'}, Leaf{i=(floor nL+1), ta=thr, m=cotArea/slaCot, a=cotArea}
+      @(rateApp lastThr pCron thr)
+    |]
 
 maintRes =
-  [rule| Cell{c=c, s=s'}, Leaf{a=a,i=i, m=m} -->
-         Cell{c=c-(maint m a i maxL nL temp)}, Leaf{m=m}
-         @1.0 [c-(maint m a i maxL nL temp) > 0] |]
+  [rule|
+    Cell{c=c, s=s'}, Leaf{a=a,i=i, m=m} -->
+    Cell{c=c-lmaint}, Leaf{m=m}
+    @1.0 [c-lmaint > 0]
+      where
+        lmaint = maint m a i maxL nL temp |]
 
 rootGrowth =
-  [rule| Root{m=m}, Cell{c=c, s=s'} -->
-         Root{m=m+ rc2m (pr * g leafMass)}, Cell{c=c-(pr* g leafMass), s=s'} @rdem thr [c-(pr*g leafMass) > 0.05*rArea] |]
+  [rule|
+    Root{m=m}, Cell{c=c, s=s'} -->
+    Root{m=m+ rc2m rg}, Cell{c=c-rg, s=s'}
+    @rdem thr [c-rg > cEqui]
+      where
+        rg = pr*g leafMass,
+        cEqui = 0.05 * rArea
+  |]
 
+md =
+    Model
+    { rules = [growth, assim, leafCr, starchConv, maintRes, rootGrowth]
+    , initState = mkSt
+    }
 
-md = Model { rules    = [growth, assim, leafCr, starchConv, maintRes, rootGrowth],
-            initState = mkSt }
+carbon =
+    Observable
+    { name = "carbon"
+    , gen = sumM c . select isCell
+    }
 
-carbon = Observable { name = "carbon",
-                      gen  = sumM c . select isCell }
+starch =
+    Observable
+    { name = "starch"
+    , gen = sumM s . select isCell
+    }
 
-starch = Observable { name = "starch",
-                      gen = sumM s . select isCell }
+m1 =
+    Observable
+    { name = "mass1"
+    , gen = sumM m . selectAttr i 1 . select isLeaf
+    }
 
-m1 = Observable { name = "mass1",
-                  gen = sumM m . selectAttr i 1 . select isLeaf }
+m2 =
+    Observable
+    { name = "mass2"
+    , gen = sumM m . selectAttr i 2 . select isLeaf
+    }
 
-m2 = Observable { name = "mass2",
-                  gen = sumM m . selectAttr i 2 . select isLeaf }
-     
-m3 = Observable { name = "mass3",
-                  gen = sumM m . selectAttr i 3 . select isLeaf }
-
-
-         
 main = runTW md tend "out/out.txt" [leafMass, rArea, carbon, nL, rootMass]
