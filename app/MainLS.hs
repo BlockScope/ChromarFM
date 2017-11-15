f2e4d0dc89ee9ee55a7a70e1a7cb7be0c97e0f09
azardilis <azardilis@gmail.com>
2017-11-15 18:11:58 +0000

    doing both outputs at once, both the counts and events

--- app/MainLS.hs
+++ app/MainLS.hs
@@ -33,23 +33,14 @@ mdSimpl e psis =
         , devfp
         , transfp
         ]
-    , initState = mkSt'' e 1 psis
+    , initState = mkSt'' e 2 psis
     }
-
-nseeds = Observable { gen=countM . select isSeed, name ="nseeds" }
-nplants = Observable { gen = countM . select isEPlant, name="nplants"}
-nfplants = Observable { gen = countM . select isFPlant, name = "nfplants"}
 
 writeOut nms fout tobss = writeFile fout (unlines rows)
   where
     header = "time" ++ "," ++ intercalate "," nms
     rows = header : [show t ++ ","  ++ showr obs | (t, obs) <- tobss]
     showr obss = intercalate "," $ map show obss
-
-writeEvents :: FilePath -> [Event] -> IO ()
-writeEvents fout events = writeFile fout (unlines rows)
-  where
-    rows = undefined
 
 gop obss tss fout md = do
   rgen <- R.getStdGen
@@ -68,10 +59,14 @@ mkEvent Rxn{lhs=[(FPlant{attr=atr}, 1)], rhs=[(Seed{}, 1)]} = Just (ind atr, See
 mkEvent _ = Nothing
 
 collectEvent :: State Agent -> Maybe Event
-collectEvent (State _ r t _) = do
+collectEvent (State m r t _) = do
   (i, eventT) <- mkEvent r
-  return $ Event { timeE=t, pid=i, typeE=eventT}
-
+  return $ Event { timeE=t, pid=i, typeE=eventT, nSeeds=nss, nPlants=nps, nFPlants=nfps}
+    where
+      nss = countMI . select isSeed $ m
+      nps = countMI . select isEPlant $ m
+      nfps = countMI . select isFPlant $ m
+      
 gopEvents fout tend md = do
     rgen <- R.getStdGen
     let traj =
@@ -90,34 +85,31 @@ tsample ts@(ts1:tss) tv@((t1, v1):(t2, v2):tvs)
     | ts1 >= t1 && ts1 < t2 = (ts1, v1) : tsample tss ((t2, v2) : tvs)
     | ts1 >= t2 = tsample ts tvs
 
+nseeds = Observable { gen=countM . select isSeed, name ="nseeds" }
+nplants = Observable { gen = countM . select isEPlant, name="nplants"}
+nfplants = Observable { gen = countM . select isFPlant, name = "nfplants"}
+
 main = do
     psis <- normalsIO
     print "running..."
-    gopEvents
-        "out/lifeExpsVal/outNsL.txt"
-        (5*365*24)
-        (mdSimpl
-             (Env
-             { psim = 0.0
-             , frepr = 0.737
-             }) (take 1 psis) )
     -- gop
     --     [nseeds, nplants, nfplants]
-    --     [0,24 .. 20*365 * 24]
+    --     [0,24 .. 60 * 365 * 24]
     --     "out/lifeExpsVal/outNsL.txt"
     --     (mdSimpl
     --          (Env
-    --          { psim = 0.0
-    --          , frepr = 0.737
-    --          }) (take 100 psis) )
-    -- print "done with L.."
-    -- gop
-    --     [nseeds, nplants, nfplants]
-    --     [0,24 .. 60*365 * 24]
-    --     "out/lifeExpsHal/outNsH.txt"
-    --     (mdSimpl
-    --          (Env
-    --          { psim = 2.5
-    --          , frepr = 0.737
-    --          }) (take 100 psis) )
-    -- print "done with H.."
+    --           { psim = 0.0
+    --           , frepr = 0.737
+    --           })
+    --          (take 2 psis))
+
+    
+    gopEvents
+        "out/lifeExpsVal/outEventsL.txt"
+        (60 * 365 * 24)
+        (mdSimpl
+             (Env
+              { psim = 0.0
+              , frepr = 0.737
+              })
+             (take 2 psis))
