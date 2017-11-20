3b28abdba7b1a2474f9ec7028854b65fdc897a63
azardilis <azardilis@gmail.com>
2017-11-20 12:16:29 +0000

    use all 60 years of weather data

--- Plant/Env.hs
+++ Plant/Env.hs
@@ -21,16 +21,18 @@ to = 22
 
 fu = 0
 
-dataFile = "data/rad/weatherValencia2yrsRad.csv"
+--dataFile = "data/rad/weatherValencia2yrsRad.csv"
+dataFile = "data/weatherValencia60yrs.csv"
 
 shift :: Time -> Fluent a -> Fluent a
 shift t0 f = mkFluent (\t -> at f (t + t0))
 
-temp' = repeatEvery 17520 (unsafePerformIO (readTable dataFile 4))
-photo' = repeatEvery 17520 (unsafePerformIO (readTable dataFile 2))
-day' = repeatEvery 17520 (unsafePerformIO (readTable dataFile 3))
-moist = repeatEvery 17520 (unsafePerformIO (readTable dataFile 5))
-par = repeatEvery 17520 (unsafePerformIO (readTable dataFile 6))
+temp' = unsafePerformIO (readTable dataFile 4)
+photo' = unsafePerformIO (readTable dataFile 2)
+day' = unsafePerformIO (readTable dataFile 3)
+moist = unsafePerformIO (readTable dataFile 5)
+--par = repeatEvery 17520 (unsafePerformIO (readTable dataFile 6))
+par = constant 120.0
 day  = day' <>*> constant 0.0
 
 -- sunrise = 6
