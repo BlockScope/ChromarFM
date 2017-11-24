9006094b2d1040d0f044c8fec2297afdc80b2e8c
azardilis <azardilis@gmail.com>
2017-11-24 17:15:18 +0000

    cheaty way to solve the running with different weather files problem. The script replaces the name of the files depending on the given location:/

---
+++ runPlant.sh
@@ -0,0 +1,26 @@
+#!/usr/local/bin/bash
+
+loc=$1;
+startTime=$2;
+endTime=$3;
+
+declare -A fNms=(["Val"]='"data\/rad\/weatherValencia2yrsRad.csv"'
+		 ["Oul"]='"data\/rad\/weatherOulu2yrsRad.csv"'
+		 ["Hal"]='"data\/rad\/weatherHalle2yrsRad.csv"'
+		 ["Nor"]='"data\/rad\/weatherNorwich2yrsRad.csv"')
+
+declare -A fOutDirs=(["Val"]='out/fmLiteExpsVal'
+		     ["Oul"]='out/fmLiteExpsOul'
+		     ["Hal"]='out/fmLiteExpsHal'
+		     ["Nor"]='out/fmLiteExpsNor')
+
+locFName="${fNms[$loc]}"
+outDir="${fOutDirs[$loc]}"
+
+sed "s/ENV/$locFName/" Plant/EnvTempl.hs | sed "s/ST/$startTime/" > Plant/Env.hs
+stack build
+echo "compiled"
+echo "stack exec -- fmLite $outDir $startTime $endTime"
+stack exec -- fmLite "$outDir" "$startTime" "$endTime"
+open "$outDir/plots/mass.png"
+
