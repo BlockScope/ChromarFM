f679dc74b2d14512ff5524253063a508d21732fc
azardilis <azardilis@gmail.com>
2018-01-24 16:27:49 +0000

    fixed some small errors in the model

--- soilWater/src/soilModel.r
+++ soilWater/src/soilModel.r
@@ -26,12 +26,23 @@ rhoSat <- function() {
     return(approxfun(x, y))
 }
 
+posorzero <- function(x) {
+    if (x < 0.0) {
+        return(0.0)
+    }
+
+    return(x)
+}
+
 wvp <- function(temp) {
     #temp is in Kelvin
+    temp <- temp + 273.15
+    t0 <- 273.15
+    
     a1 <- 611.21
     a3 <- 17.502
     a4 <- 32.19
-    svp <- a1 * exp(a3 * (t - t0)/(t-a4))
+    svp <- a1 * exp(a3 * (temp - t0)/(temp-a4))
 
     return(svp)
 }
@@ -52,23 +63,23 @@ revap <- function(temp, rad) {
 
 resAir <- function(h, k) {
     z <- 0.01
-    return((log(h/z)**2) / (k**2*0.02))
+    return((log(h/z)**2) / (k**2*0.3))
 }
 
 wvEvap <- function(temp, dtemp) {
-    rAir <-  resAir(10, 0.4)
+    rAir <-  resAir(5, 0.4)
     rhos <- rhoSat()(temp)
     st <- s()(temp)
     gt <- gm()(temp)
 
     
-    ewv <- 86400*(rhos * (1-rh(temp, dtemp)) / (rAir * (1 + (st / gt)))
+    ewv <- 86400*(rhos * (1-rh(temp, dtemp))) / (rAir * (1 + (st / gt)))
 
     return(ewv * (-1))
 }
 
 mkYearSoil <- function(wsoil, env) {
-    ndays <- length(prec)
+    ndays <- length(env$rad)
     soilWater <- rep(0, ndays)
     wsoilMax <- 400
     
@@ -76,25 +87,25 @@ mkYearSoil <- function(wsoil, env) {
         hdrys <- (wsoilMax - wsoil) / 1000
         fssev <- 0.02 / (0.02 + hdrys)
 
-        evapTotal <- revap(env$temp[i], env$rad[i]) +
-                     wvEvap(env$temp[i], env$dtemp[i])
-        dw <- env$prec[i] + (evapTotal * fssev)
-        if (wsoil >= wsoilmax && dw > 0) {
+        evapTotal <- revap(env$temps[i], env$rads[i]) +
+                     wvEvap(env$temps[i], env$dtemps[i])
+        dw <- env$precs[i] + (evapTotal * fssev)
+        if (wsoil >= wsoilMax && dw > 0) {
             wsoil <- wsoil - dw
         } else {
             wsoil <- posorzero(wsoil + dw)
         }
-        soilwater[i] <- posorzero(wsoil)
+        soilWater[i] <- posorzero(wsoil)
     }
 
-    return(soilwater)
+    return(soilWater)
 }
 
 mkSoilWater <- function(env) {
-    ndays <- length(prec)
-    sw <- mkYearSoil(400, prec,eva)
-    sw1 <- mkYearSoil(sw[ndays], prec,eva)
-    sw2 <- mkYearSoil(sw1[ndays], prec,eva)
+    ndays <- length(env$prec)
+    sw <- mkYearSoil(400, env)
+    sw1 <- mkYearSoil(sw[ndays], env)
+    sw2 <- mkYearSoil(sw1[ndays], env)
 
     return(sw2)
 }
