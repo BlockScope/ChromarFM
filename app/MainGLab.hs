e943794af8e8d4ebcec70f56ef245c2d4fb4186e
azardilis <azardilis@gmail.com>
2018-08-13 17:13:27 +0100

    added functions for handling the structure

--- app/MainGLab.hs
+++ app/MainGLab.hs
@@ -9,7 +9,7 @@ import           Data.Colour.Names
 import           Data.Default.Class
 import           Data.List
 import           Env
-import           GHC.Exts                               (groupWith, the)
+import           GHC.Exts                               (groupWith, the, sortWith)
 import           Graphics.Rendering.Chart
 import           Graphics.Rendering.Chart.Backend.Cairo
 import           Plant
@@ -30,7 +30,7 @@ mkSt' = ms
     fR = rdem 0.0 thrmFinal
     ra = 2*cotArea*cos (10/180*pi)
     si = initS * initC * ra
-    atr = Attrs{ind=1, psi=0.0, fi=0.737}
+    atr = Attrs{ind=1, psi=0.0, fi=0.598}
 
 mkSt'' :: Multiset Agent
 mkSt'' = ms [Plant{attr=atr, thrt=1.0, dg=0.0, wct=0.0}]
@@ -67,15 +67,17 @@ mdLiteGreenlab =
               fruitGrowth,
               leafD,
               leafD',
-              starchConvRepr]
-          ,initState= mkSt' }
+              starchConvRepr,
+              eme,
+              devp]
+          ,initState= mkSt'' }
 
 main' = do
-    let dur = (263+47)*24
+    let dur = (160+57)*24
         outDir = "out/greenlabExps"
     print "running"    
     goPlot'
-        2
+        5
         [ carbon
         , leafMass
         , starch
@@ -117,11 +119,47 @@ main' = do
         outDir
         mdLiteGreenlab
         (\s -> hasSSeeds (getM s))
+        
+type Mass = Double
+
+data AgentS
+  = LeafS Int
+          Mass
+  | LLeafS Int
+           Par
+           Mass
+  | FruitS Par
+           Mass
+  deriving (Eq, Show)
+
+toAgentS :: Agent -> [AgentS]
+toAgentS Leaf{i=i, m=m} = [LeafS i m]
+toAgentS LLeaf{lid=i, lm=m, pll=p} = [LLeafS i p m]
+toAgentS Fruit{pf=p, fm=m} = [FruitS p m]
+toAgentS _ = []
+
+getOrd :: AgentS -> Int
+getOrd (LeafS i _) = i
+getOrd (LLeafS _ (L i) _) = i
+getOrd (LLeafS _ (V i) _) = i
+getOrd (FruitS (L i) _) = i
+getOrd (FruitS (V i) _) = i
+
+printStruct :: Multiset Agent -> IO ()
+printStruct m = mapM_ print $ sortWith getOrd (concatMap toAgentS (toList m))
+
+runTWStruct :: Model Agent -> Time -> FilePath -> [Observable Agent] -> IO ()
+runTWStruct (Model {rules = rs
+                   ,initState = s}) t fn obss = do
+    rgen <- R.getStdGen
+    let traj = takeWhile (\s -> getT s < t) (simulate rgen rs s)
+    writeObs fn obss traj
+    printStruct (getM $ last traj)
 
 mainRun = do
     let outDir = "out/greenlabExps"
     print "running"
-    runTW
+    runTWStruct
         mdLiteGreenlab
         1500
         "out/greenlabExps/text/out.txt"
@@ -164,4 +202,4 @@ mainRun = do
         , totalFMass  
         ] 
 
-main = main'
+main = mainRun
